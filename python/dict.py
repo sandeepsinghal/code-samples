f414b549d09f1c47d2afc865fba6a823e1ab3de5
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-12-26 18:14:42 +0530

    adding dictionary sample

---
+++ python/dict.py
@@ -0,0 +1,16 @@
+__author__ = 'ssinghal'
+
+word_counts = {"sandeep" : 7, "sam" : 10, "sid" : 9, "ruby":8}
+
+print word_counts["ruby"]
+
+# Check if a key exists in the dictionary
+if "sid" in word_counts:
+    print "present"
+
+#def awesome_count(dict):
+
+#    if 'awesome' in dict
+#        return dict['awesome']
+#    else
+#        return 0
